f3e7516407e523864e6428a7e86dc58e2a2f97a3
Melania Chiru <melania@chiru.ca>
2025-01-18 15:10:36 -0500

    feat: create component for the level page

--- App.jsx
+++ App.jsx
@@ -2,16 +2,26 @@ const App = () => {
 	let icons = Level1();
 	return ( 
 		<>
+<<<<<<< HEAD
 			<WelcomePage />
 			<Header level="1"/>
+=======
+			{/* <Header level="1"/>
+>>>>>>> 541229c (feat: create component for the level page)
 			<Tile id={1} icon="/icons/water_drop.svg"></Tile>
 			<Plant />
 
+<<<<<<< HEAD
 			<Resources/>
 			<Rules />
+=======
+			<Resources/> */}
+			<LevelPage></LevelPage>
+>>>>>>> 541229c (feat: create component for the level page)
 		</>
 	 );
 }
 
 //render content
 ReactDOM.render(<App />,document.getElementById("body"));
+
