541229cba8e5e21a4912b86a6fc491073fb217c9
Melania Chiru <melania@chiru.ca>
2025-01-18 15:10:36 -0500

    feat: create component for the level page

---
+++ components/LevelPage.jsx
@@ -0,0 +1,15 @@
+const LevelPage = () => {
+	const {tiles, resources} = Level1();	
+	
+	return ( 
+		<div className="level-page">
+			<div className="main-column">
+				<Header level="1"></Header>
+				<Grid tiles={tiles}></Grid>
+			</div>
+			<div className="side-column">
+				<Resources resources={resources}></Resources>
+			</div>
+		</div>
+	 );
+}
