d66ae0c0cf792176c2ca998d1298f6a8d664daa9
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-18 12:46:39 -0500

    add icon display array

--- components/Resources.jsx
+++ components/Resources.jsx
@@ -1,9 +1,15 @@
 const Resources = (props) => {
+    let iconList = props.icons;
     return ( 
         <>
             <h2>Resources</h2>
             <sections id="sec-icon">
-
+                {iconList.map((icon)=>(
+                    <section class="icon-sec">
+                        <section class="icon">{icon.icon}</section>
+                        <section class="qty">x{icon.qty}</section>
+                    </section>
+                ))}
             </sections>
         </>
      );
