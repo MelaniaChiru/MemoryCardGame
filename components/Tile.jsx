1f67477d16a9c1b565d03bff63197c5c2527ecd1
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-18 17:02:57 -0500

    add onClick props

--- components/Tile.jsx
+++ components/Tile.jsx
@@ -1,7 +1,10 @@
 const Tile = (props) => {
 	return ( 
-		<div className="tile" id={props.id}>
-			<img src={props.icon}/>
+		<div className="tile" id={props.id} onClick={props.onClick} >
+			<img src={props.icon} class="hidden"/>
 		</div>
 	 );
+
+	 
+
 }
