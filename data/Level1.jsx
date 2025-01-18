c58db585648117612b64cad51bd1efc86211c390
Melania Chiru <melania@chiru.ca>
2025-01-18 15:06:41 -0500

    feat: add all data for level 1 to file

--- data/Level1.jsx
+++ data/Level1.jsx
@@ -1,8 +1,17 @@
 const Level1 = () => {
-    let iconList = [
-        {icon: "icons/water_drop.svg", qty: 1},
-        {icon: "icons/water_drop.svg", qty: 1},
-        {icon: "icons/water_drop.svg", qty: 1}
-    ];
-    return (iconList);
+    return {
+        tiles: [
+            {icon: "icons/water_drop.svg", id: 1, nbTiles: 2},
+            {icon: "icons/water_drop.svg", id: 2, nbTiles: 2},
+            {icon: "icons/water_drop.svg", id: 3, nbTiles: 2},
+            {icon: "icons/water_drop.svg", id: 4, nbTiles: 1},
+        ],
+        resources: [
+            {icon: "icons/water_drop.svg", qty: 1},
+            {icon: "icons/water_drop.svg", qty: 1},
+            {icon: "icons/water_drop.svg", qty: 1}
+        ]
+    }
+    // let iconList = 
+    // return (iconList);
 }
