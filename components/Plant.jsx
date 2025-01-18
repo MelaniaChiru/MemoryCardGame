797aebbb0f0d83f225e036e790b475e50bd6c20b
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-18 15:03:14 -0500

    add plant image to tag

--- components/Plant.jsx
+++ components/Plant.jsx
@@ -2,10 +2,10 @@ const  Plant = () => {
 
     return (
         <>
-            <img src="image/level1.1.svg" class="stage"></img>
-            <img src="image/level1.2.svg" display="none" class="stage"></img>
-            <img src="image/level1.3.svg" display="none" class="stage"></img>
-            <img src="image/level1.4.svg" display="none" class="stage"></img>
+            <img src="image/level1.1.svg"></img>
+            <img src="image/level1.2.svg" class="hidden"></img>
+            <img src="image/level1.3.svg" class="hidden"></img>
+            <img src="image/level1.4.svg" class="hidden"></img>
         </>
     );
 }
