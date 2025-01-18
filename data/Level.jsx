916f667b12b63f801b4beee1b967a3f244bde1a7
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-18 12:28:56 -0500

    add data to level1

---
+++ data/Level.jsx
@@ -0,0 +1,8 @@
+const Level = () => {
+    let iconList = [
+        {icon: "icons/water_drop.svg", qty: 1},
+        {icon: "icons/water_drop.svg", qty: 1},
+        {icon: "icons/water_drop.svg", qty: 1}
+    ];
+    return (iconList);
+}
