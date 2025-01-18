d4484e0cad2f80d549d5ad4dd54961bdf42f7fbf
Melania Chiru <melania@chiru.ca>
2025-01-18 12:35:48 -0500

    feat: define tile component

---
+++ App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+	return ( 
+		<>
+			<Tile id={1} icon="/icons/water_drop.svg"></Tile>
+		</>
+	 );
+}
+
+//render content
+ReactDOM.render(<App />,document.getElementById("body"));
