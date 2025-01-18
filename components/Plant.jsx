fcf14dc2e9f945c8594ee2f3b67957d330186aba
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-18 14:55:26 -0500

    add photos

---
+++ components/Plant.jsx
@@ -0,0 +1,11 @@
+const  Plant = () => {
+
+    return (
+        <>
+            <img src="image/level1.1.svg" class="stage"></img>
+            <img src="image/level1.2.svg" display="none" class="stage"></img>
+            <img src="image/level1.3.svg" display="none" class="stage"></img>
+            <img src="image/level1.4.svg" display="none" class="stage"></img>
+        </>
+    );
+}
