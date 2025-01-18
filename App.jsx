116870ab12ceec7b657639770b09228b994ce5e1
Melania Chiru <melania@chiru.ca>
2025-01-18 15:14:25 -0500

    feat: display proprer number of tiles

--- App.jsx
+++ App.jsx
@@ -2,10 +2,13 @@ const App = () => {
 	let icons = Level1();
 	return ( 
 		<>
+			<WelcomePage />
 			<Header level="1"/>
 			<Tile id={1} icon="/icons/water_drop.svg"></Tile>
+			<Plant />
 
 			<Resources/>
+			<Rules />
 		</>
 	 );
 }
---
+++ components/Plant.jsx
@@ -0,0 +1,11 @@
+const  Plant = () => {
+
+    return (
+        <>
+            <img src="image/level1.1.svg"></img>
+            <img src="image/level1.2.svg" class="hidden"></img>
+            <img src="image/level1.3.svg" class="hidden"></img>
+            <img src="image/level1.4.svg" class="hidden"></img>
+        </>
+    );
+}
