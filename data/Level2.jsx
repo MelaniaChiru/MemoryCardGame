ed8a8035b5b95cce3233d50c099f376ea1f0e430
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-19 09:58:56 -0500

    add data

---
+++ data/Level2.jsx
@@ -0,0 +1,15 @@
+const Level2 = () => {
+    return {
+        tiles: [
+            {icon: "/icons/water_drop.svg", id: 1, nbTiles: 2},
+            {icon: "/icons/tool.svg", id: 2, nbTiles: 2},
+            {icon: "/icons/sun.svg", id: 3, nbTiles: 2},
+            {icon: "/icons/dirt.svg", id: -1, nbTiles: 3}
+        ],
+        resources: [
+            {icon: "/icons/water_drop.svg", qty: 1},
+            {icon: "/icons/tool.svg", qty: 1},
+            {icon: "/icons/sun.svg", qty: 1}
+        ]
+    }
+}
