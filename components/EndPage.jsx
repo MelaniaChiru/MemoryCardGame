6f19305334d5d799f9ea5d81aa06721536af7b56
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-19 09:22:26 -0500

    add header and buttons

--- components/EndPage.jsx
+++ components/EndPage.jsx
@@ -1,7 +1,9 @@
 const EndPage = () => {
     return ( 
         <section id="endPage">
-            
+            <h1>Level Complete!</h1>
+            <button>Play Again</button>
+            <button>Next Level</button>
         </section>
      );
 }
