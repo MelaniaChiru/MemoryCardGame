cf7568fec5f504308042eace01ca7c83445782fa
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-19 09:59:54 -0500

    change data to fit level 2

--- data/Level2.jsx
+++ data/Level2.jsx
@@ -1,15 +1,15 @@
 const Level2 = () => {
     return {
         tiles: [
-            {icon: "/icons/water_drop.svg", id: 1, nbTiles: 2},
-            {icon: "/icons/tool.svg", id: 2, nbTiles: 2},
-            {icon: "/icons/sun.svg", id: 3, nbTiles: 2},
-            {icon: "/icons/dirt.svg", id: -1, nbTiles: 3}
+            {icon: "/icons/water_drop.svg", id: 1, nbTiles: 4},
+            {icon: "/icons/tool.svg", id: 2, nbTiles: 4},
+            {icon: "/icons/sun.svg", id: 3, nbTiles: 4},
+            {icon: "/icons/dirt.svg", id: -1, nbTiles: 4}
         ],
         resources: [
-            {icon: "/icons/water_drop.svg", qty: 1},
-            {icon: "/icons/tool.svg", qty: 1},
-            {icon: "/icons/sun.svg", qty: 1}
+            {icon: "/icons/water_drop.svg", qty: 2},
+            {icon: "/icons/tool.svg", qty: 2},
+            {icon: "/icons/sun.svg", qty: 2}
         ]
     }
 }
