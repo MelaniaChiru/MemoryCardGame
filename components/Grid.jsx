71dbd59857f7349c45318bb23d4d2fcaa24d5ac1
Melania Chiru <melania@chiru.ca>
2025-01-19 09:09:36 -0500

    feat: when a pair is matched, change corresponding icon to coloured version

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,13 +1,18 @@
-const Grid = ({tiles}) => {
+const Grid = ({tiles, resources, setResources}) => {
 	let count = 0;
 	let t = [];
 	let eventTile = [];
+
 	return ( 
 		<div className="tiles-grid">
 			{hydrateTiles()}
 		</div>
 	);
 
+	/**
+	 * takes array of tiles from data file, shuffles them and displays them
+	 * @returns array of Tiles
+	 */
 	function hydrateTiles(){
 		let allTiles = [];
 		
@@ -23,45 +28,6 @@ const Grid = ({tiles}) => {
 			return (<Tile onClick={(e)=>{tileHandler(e)}} id={tile.id} icon={tile.icon}></Tile>);
 		});
 
-		
-		function tileHandler(e) {
-			if(count < 2) {
-				e.target.children[0].classList.remove("hidden");
-				count = count + 1;
-				eventTile.push(e);
-				t.push(e.target.id);
-			}
-
-			if(count == 2) {
-				checkTiles();
-			}
-		}
-
-		function checkTiles() {
-			if(eventTile[0].target.id == eventTile[1].target.id && eventTile[0].target.id < 0) {
-				setTimeout(()=> {
-					eventTile[0].target.children[0].classList.add("hidden");
-					eventTile[1].target.children[0].classList.add("hidden");
-					count = 0;
-					eventTile = [];
-				}, 500);
-			}else if(eventTile[0].target.id == eventTile[1].target.id) {
-				console.log("it worked");
-				count = 0;
-				eventTile = [];
-			}else {
-				setTimeout(()=> {
-					eventTile[0].target.children[0].classList.add("hidden");
-					eventTile[1].target.children[0].classList.add("hidden");
-					count = 0;
-					eventTile = [];
-				}, 500);
-			}
-		}
-
-
-
-
 		/**
 		 * 'Shuffles' the tiles, so the order is different
 		 * @param {Array} tiles array of the all the tiles
@@ -74,6 +40,85 @@ const Grid = ({tiles}) => {
 			}
 			return tiles;
 		}
-		
 	}
+
+	function tileHandler(e) {
+		if(count < 2) {
+			e.target.children[0].classList.remove("hidden");
+			count = count + 1;
+			eventTile.push(e);
+			t.push(e.target.id);
+		}
+
+		if(count == 2) {
+			checkTiles();
+		}
+	}
+
+	function checkTiles() {
+		if(eventTile[0].target.id == eventTile[1].target.id && eventTile[0].target.id < 0) {
+			setTimeout(()=> {
+				eventTile[0].target.children[0].classList.add("hidden");
+				eventTile[1].target.children[0].classList.add("hidden");
+				count = 0;
+				eventTile = [];
+			}, 500);
+		}else if(eventTile[0].target.id == eventTile[1].target.id) {
+			//colour appropriate icons in resources
+			fillIcons(eventTile[0].target.id);
+
+			count = 0;
+			eventTile = [];
+		}else {
+			setTimeout(()=> {
+				eventTile[0].target.children[0].classList.add("hidden");
+				eventTile[1].target.children[0].classList.add("hidden");
+				count = 0;
+				eventTile = [];
+			}, 500);
+		}
+	}
+
+	/**
+	 * When a pair of tiles is matched, the corresponding resource icon is filled in with colour
+	 * @param {String} id id of the tiles that were just matched
+	 */
+	function fillIcons(id){
+		if (id === '1'){ // water
+			setResources(prevResources => {
+				return prevResources.map(resource => {
+					if (resource.icon === "/icons/water_drop_gray.svg") {
+						// Change the water icon only
+						return { ...resource, icon: "/icons/water_drop.svg" };
+					}
+					return resource; 
+				});
+			});
+		}
+
+		if (id === '2'){ // tool
+			setResources(prevResources => {
+				return prevResources.map(resource => {
+					if (resource.icon === "/icons/tool_gray.svg") {
+						// Change the tool icon only
+						return { ...resource, icon: "/icons/tool.svg" };
+					}
+					return resource; 
+				});
+			});
+		}
+
+		if (id === '3'){ // sun
+			setResources(prevResources => {
+				return prevResources.map(resource => {
+					if (resource.icon === "/icons/sun_gray.svg") {
+						// Change the sun icon only
+						return { ...resource, icon: "/icons/sun.svg" };
+					}
+					return resource; 
+				});
+			});
+		}
+		
+    }
 }
