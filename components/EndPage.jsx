f034a770d65544baf5e8918c302b34d8edcf7040
Ash Rebelo <ashrebelo21@gmail.com>
2025-01-19 14:29:26 -0500

    add varible to button

--- components/EndPage.jsx
+++ components/EndPage.jsx
@@ -1,9 +1,23 @@
-const EndPage = ({classes, playAgain, nextLevel}) => {
+const EndPage = ({classes, playAgain, nextLevel, prevLevel, gameLevel}) => {
+    let netLvlBtn;
+    let prevLvlBtn;
+
+    if(gameLevel == 1) {
+        netLvlBtn = "button endPageBtn";
+        prevLvlBtn = "hidden";
+
+    }else {
+        prevLvlBtn = "button endPageBtn"
+        netLvlBtn = "hidden";
+    }
+
+
     return ( 
         <section id="endPage" className={classes}>
             <h1>Level Complete!</h1>
-            <button class="button" onClick={playAgain}>Play Again</button>
-            <button class="button" onClick={nextLevel}>Next Level</button>
+            <button class="button endPageBtn" onClick={playAgain}>Play Again</button>
+            <button class={netLvlBtn} onClick={nextLevel}>Next Level</button>
+            <button class={prevLvlBtn} onClick={prevLevel}>Previous Level</button>
         </section>
      );
 }
