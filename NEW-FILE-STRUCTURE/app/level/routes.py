0649ea2f9614b09c337804757e80980b5c7ad99f
Melania Chiru <melania@chiru.ca>
2025-06-07 21:18:03 -0400

    feat: define basic routes and register blueprints

--- NEW-FILE-STRUCTURE/app/level/routes.py
+++ NEW-FILE-STRUCTURE/app/level/routes.py
@@ -10,7 +10,7 @@ def levels():
 	# ''' renders the level selection menu page '''
 	return render_template('levels.html')
 
-@level_bp.route('/<int:level_id>')
-def level(level_id=None):
+@level_bp.route('/<int:level>')
+def level(level):
 	''' renders page for a specific level '''
-	return render_template('level.html', level_id=level_id)
+	return render_template('level.html', level=level)
