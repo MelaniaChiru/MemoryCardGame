a6615f138f9497a0f552e07b7a380f392e51af08
Ash Rebelo <ashrebelo21@gmail.com>
2025-06-08 18:27:27 -0400

    write text_to_level_object

--- NEW-FILE-STRUCTURE/app/api/level.py
+++ NEW-FILE-STRUCTURE/app/api/level.py
@@ -6,8 +6,14 @@ def all_levels():
     return text_to_level_object()
 
 def text_to_level_object():
-    
+    levels = []
+    content = read_from_file()
+    for obj in content:
+        info = obj.split(",")
+        levels.append(Level(**info))
+    return levels
 
+import csv
 def read_from_file():
     file = open("info_levels.txt", "r")
     content = file.read()
