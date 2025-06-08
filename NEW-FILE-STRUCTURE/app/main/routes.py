d80416d01d03a6368ba6691d17dacb2e6975ddc5
Melania Chiru <melania@chiru.ca>
2025-06-07 23:39:48 -0400

    refactor: rename main blueprint to ‘main_bp’

--- NEW-FILE-STRUCTURE/app/main/routes.py
+++ NEW-FILE-STRUCTURE/app/main/routes.py
@@ -3,10 +3,10 @@
 '''
 
 from flask import render_template
-from . import main
+from . import main_bp
 
-@main.route('/')
-@main.route('/home')
+@main_bp.route('/')
+@main_bp.route('/home')
 def home():
     ''' renders the home page '''
     return render_template('home.html')
