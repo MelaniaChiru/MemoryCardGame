28bce9eb4c2457933ce60bcde4307b3747b01b72
Ash Rebelo <ashrebelo21@gmail.com>
2025-06-12 13:53:27 -0400

    change dirt to grass

--- NEW-FILE-STRUCTURE/model/Level.py
+++ NEW-FILE-STRUCTURE/model/Level.py
@@ -1,5 +1,5 @@
 class Level():
-    def __init__(self, level_num, name, row, column, sun, water, tractor, cloud, bug, scissor, dirt):
+    def __init__(self, level_num, name, row, column, sun, water, tractor, cloud, bug, scissor, grass):
         self.level_num = level_num
         self.name = name
         self.row = row
@@ -10,7 +10,7 @@ class Level():
         self.cloud = cloud
         self.bug = bug
         self.scissor = scissor
-        self.dirt = dirt
+        self.grass = grass
     
     def level_id(self):
         return self.level_num
