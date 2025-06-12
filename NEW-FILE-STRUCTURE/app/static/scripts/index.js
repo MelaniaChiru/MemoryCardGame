e5919d69ad673e6fe446691ecdcb4b82f3343e47
Melania Chiru <melania@chiru.ca>
2025-06-12 19:56:48 -0400

    feat: only call flipTile() function when needed

--- NEW-FILE-STRUCTURE/app/static/scripts/index.js
+++ NEW-FILE-STRUCTURE/app/static/scripts/index.js
@@ -1,3 +1,11 @@
-// import from _level.js function called displayAlert
 import { flipTile } from './_level.js';
-flipTile();
+
+/**
+ * Only call filpTile() function if active page is a specific level page
+ * /levels/2 -- calls function
+ * /home -- doesn't call function 
+ * /levels/ -- doesn't call function  
+ */
+if (/^\/levels\/\d+$/.test(window.location.pathname)) {
+	flipTile();
+}
