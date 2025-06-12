a210920e9666683782b37a8bd76d2753c42d0745
Ash Rebelo <ashrebelo21@gmail.com>
2025-06-12 11:12:32 -0400

    change grid_size to row and columns

--- NEW-FILE-STRUCTURE/model/Level.py
+++ NEW-FILE-STRUCTURE/model/Level.py
@@ -1,8 +1,9 @@
 class Level():
-    def __init__(self, level_num, name, gird_size, sun, water, tractor, cloud, bug, scissor, dirt):
+    def __init__(self, level_num, name, row, column, sun, water, tractor, cloud, bug, scissor, dirt):
         self.level_num = level_num
         self.name = name
-        self.gird_size = gird_size
+        self.row = row
+        self.column = column
         self.sun = sun
         self.water = water
         self.tractor = tractor
