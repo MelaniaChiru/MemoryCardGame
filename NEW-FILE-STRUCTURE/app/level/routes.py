7d1da085435363b82304510090a5e07ee2f312d6
Melania Chiru <melania@chiru.ca>
2025-06-12 20:42:31 -0400

    feat: update levels route to fetch all levels from api

--- NEW-FILE-STRUCTURE/app/level/routes.py
+++ NEW-FILE-STRUCTURE/app/level/routes.py
@@ -1,6 +1,8 @@
 '''
 	Routes for the level module.
 '''
+import requests;
+from model import Level
 
 from flask import render_template
 from . import level_bp
@@ -8,7 +10,10 @@ from . import level_bp
 @level_bp.route('/')
 def levels():
 	# ''' renders the level selection menu page '''
-	return render_template('levels.html')
+	response = requests.get("/api/levels")
+	levels_data = response.json()
+	levels = [Level(**level_dict) for level_dict in levels_data]
+	return render_template('levels.html', levels = levels)
 
 @level_bp.route('/<int:level>')
 def level(level):
