3b64e8bb4b1f235c2abdac709a09b9e7c15f05fa
Ash Rebelo <ashrebelo21@gmail.com>
2025-06-15 12:23:40 -0400

    add content to level_by_num()

--- NEW-FILE-STRUCTURE/app/api/routes.py
+++ NEW-FILE-STRUCTURE/app/api/routes.py
@@ -8,9 +8,11 @@ def all_levels():
 
 @api_bp.route('levels/<int:level_num>', methods=['GET'])
 def level_by_num(level_num):
-    
-    
-    return ""
+    levels = read_from_file()
+    for level in levels:
+        if level.level_num == level_num:
+            return jsonify(level), 200
+    return "Level not found", 404
 
 import json
 def read_from_file():
